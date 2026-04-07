f7956f5165f7fa97adbafd2742a6c58233e7ef98
fatimarezaie256 <fatimarezaie112@gmail.com>
2026-04-07 19:27:17 +0430

    components have been created

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,6 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
         <title>Laravel</title>
 
         <!-- Fonts -->
@@ -138,7 +137,7 @@
                     <footer class="py-16 text-center text-sm text-black dark:text-white/70">
                         Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})
                     </footer>
-                </div>
+                 </div>
             </div>
         </div>
     </body>
