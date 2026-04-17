0156ad4f0cd5a4b245edf9e1a874ff3127c549bf
fatimarezaie256 <fatimarezaie112@gmail.com>
2026-04-17 10:46:59 +0430

    Routes have been made

--- routes/web.php
+++ routes/web.php
@@ -13,3 +13,15 @@ Route::view('profile', 'profile')
     ->name('profile');
 
 require __DIR__.'/auth.php';
+Route::prefix('/dashboard')->middleware(['auth', 'verified'])->group(function (){
+    Route::view('/','dashboard');
+    Route::view('profile','profile');
+   Route::view('employee','livewire.employee.index');
+   Route::view('contract','livewire.contract.index');
+   Route::view('school','livewire.school.index');
+   Route::view('department','livewire.department.index');
+   Route::view('designation','livewire.designation.index');
+   Route::view('salary','livewire.salary.index');
+   Route::view('payment','livewire.payment.index');
+   Route::view('payroll','livewire.payroll.index');
+});
