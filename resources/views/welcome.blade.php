645bb127eeaf5866510d1fadc39c680367d4cd10
fatimarezaie256 <fatimarezaie112@gmail.com>
2026-04-23 15:30:11 +0430

    view todo created

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,7 +6,6 @@
         <title>Laravel</title>
 
         <!-- Fonts -->
-        @vite('resources/js/slider.js')
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
